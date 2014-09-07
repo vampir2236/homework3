d4f5a363437942ba852f08fa056b44b21f77e84f
vampir2236 <vampir2236@mail.ru>
2014-09-08 00:41:19 +0400

    первый коммит

---
+++ gulpfile.js
@@ -0,0 +1,70 @@
+/*global require, console*/
+(function () {
+    'use strict';
+
+    var gulp = require('gulp'),
+        less = require('gulp-less'),
+        notify = require('gulp-notify'),
+        concatCss = require('gulp-concat-css'),
+        prefixer = require('gulp-autoprefixer'),
+        minifyCss = require('gulp-minify-css'),
+        rename = require('gulp-rename'),
+        concat = require('gulp-concat'),
+        uglify = require('gulp-uglify');
+
+
+    /* less */
+    gulp.task('less', function () {
+        gulp.src('./source/less/main.less')
+            .pipe(less())
+            .on('error', function () {
+                console.log('Ошибка в задаче less!');
+            })
+            .pipe(gulp.dest('./source/css'))
+            .pipe(notify('less  Ok!'));
+    });
+
+
+    /* css */
+    gulp.task('css', function () {
+        gulp.src(['./bower_components/normalize-css/normalize.css',
+                 './source/css/main.css'])
+            .pipe(concatCss('all.css'))
+            .on('error', function () {
+                console.log('Ошибка в задаче css!');
+            })
+            .pipe(prefixer())
+            .pipe(minifyCss())
+            .pipe(rename({
+                suffix: '.min'
+            }))
+            .pipe(gulp.dest('./app/css'))
+            .pipe(notify('css  Ok!'));
+    });
+
+
+    /* js */
+    gulp.task('js', function () {
+        gulp.src(['./bower_components/bootstrap/dist/js/bootstrap.min.js',
+                './source/js/main.js'])
+            .pipe(concat('all.js'))
+            .pipe(uglify())
+            .pipe(rename({
+                suffix: '.min'
+            }))
+            .pipe(gulp.dest('./app/js'))
+            .pipe(notify('js  Ok!'));
+    });
+
+
+    /* watch */
+    gulp.task('watch', function () {
+        gulp.watch('./source/less/main.less', ['less']);
+        gulp.watch('./source/css/*.css', ['css']);
+        gulp.watch('./source/js/*.js', ['js']);
+    });
+
+
+    gulp.task('default', ['watch']);
+
+})();
