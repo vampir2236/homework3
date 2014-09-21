0de6ad90410e52d486ef1a8d551a4fa35c1ba348
vampir2236 <vampir2236@mail.ru>
2014-09-21 22:40:18 +0400

    1. Убрал outline у элементов (buttons, inputs). 2. Добавил текст поумолчанию в кнопку. 3. Добавил tooltip. 4. В js кеширую this. 5. Пофиксил ie8:   а) Добавил вертикальный градиент для кнопок   б) Поправил стиль у кнопок в ie8 отображались не по центру контейнера

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,5 @@
-/*global jQuery*/
-(function ($) {
+/*global jQuery, window*/
+(function ($, window) {
     'use strict';
 
     var app = {
@@ -19,56 +19,86 @@
         $outputHTML: $('.js-output-html'),
         $outputCSS: $('.js-output-css'),
         $outputEmail: $('.js-email'),
-        $outputMsg: $('.js-output-msg'),
         br: 0,
         bw: 0,
 
 
         /* инициализация объекта*/
         init: function () {
-            $(this.$borderRadius).empty().slider({
+            var that = this;
+
+            $(that.$borderRadius).empty().slider({
                 range: 'min',
                 value: 30
             });
-            $(this.$borderWidth).empty().slider({
+            $(that.$borderWidth).empty().slider({
                 range: 'min',
                 value: 20
             });
+            that.$outputEmail.tooltip({
+                position: {
+                    my: 'center bottom',
+                    at: 'center top-10'
+                },
+                items: 'input',
+                show: false,
+                hide: false,
 
-            this.$borderRadius.trigger('slidechange');
-            this.$borderWidth.trigger('slidechange');
-            this.$btnText.trigger('keyup');
+                /*событие для закрытия тултипа по нажатию*/
+                open: function () {
+                    $('.ui-tooltip').on('click', function () {
+                        that.hideTooltip();
+                    });
+                }
+            });
+
+            that.$borderRadius.trigger('slidechange');
+            that.$borderWidth.trigger('slidechange');
+            that.$btnText.trigger('keyup');
 
             $('input, textarea').placeholder();
 
-            this.setUpListeners();
+            that.setUpListeners();
 
-            this.$borderRadius.trigger('slidechange');
-            this.$borderWidth.trigger('slidechange');
-            this.$btnText.trigger('change');
+            that.$borderRadius.trigger('slidechange');
+            that.$borderWidth.trigger('slidechange');
+            that.$btnText.trigger('change');
         },
 
 
         /* обработчики событий */
         setUpListeners: function () {
-            this.$form.on('submit', $.proxy(this.submitForm, this));
-            this.$borderRadius.on('slidechange', $.proxy(this.changeBorderRadius, this));
-            this.$borderWidth.on('slidechange', $.proxy(this.changeBorderWidth, this));
-            this.$btnText.on('change keyup input', $.proxy(this.changeButtonText, this));
-            this.$outputEmail.on('change keyup input', $.proxy(this.hideMsg, this));
+            var that = this;
+
+            that.$form.on('submit', $.proxy(that.submitForm, that));
+            that.$borderRadius.on('slidechange', $.proxy(that.changeBorderRadius, that));
+            that.$borderWidth.on('slidechange', $.proxy(that.changeBorderWidth, that));
+
+            /* fix для мобильных ловим change, keyup, input */
+            that.$btnText.on('change keyup input', $.proxy(that.changeButtonText, that));
+            that.$outputEmail.on('change keyup input', $.proxy(that.hideTooltip, that));
+
+            /* fix при изменении размеров окна тултип слетал */
+            $(window).on('resize', $.proxy(that.refreshTooltip, that));
+
+//            on('click', function () {
+//                //this.hideTooltip();
+//                console.log('asd');
+//            });
         },
 
 
         /* методы */
         submitForm: function (event) {
-            var $submitBtn = this.$form.find('button[type="submit"]'),
-                str = this.$form.serialize();
+            var that = this,
+                $submitBtn = that.$form.find('button[type="submit"]'),
+                str = that.$form.serialize();
 
             event.preventDefault();
-            this.hideMsg();
+            that.hideTooltip();
 
-            if (!/.+@.+\..+/.test(this.$outputEmail.val())) {
-                this.showMsg('Адрес почты введён неверно.', true);
+            if (!/.+@.+\..+/.test(that.$outputEmail.val())) {
+                that.showTooltip('E -mail address you entered is invalid!', true);
                 return;
             }
 
@@ -77,14 +107,14 @@
             $.ajax({
                 url: 'php/sendmail.php',
                 type: 'POST',
-                context: this,
+                context: that,
                 data: str
             })
                 .done(function (data) {
-                    this.showMsg('Сообщение отправлено.');
+                    this.showTooltip('Message sent.');
                 })
                 .fail(function (data) {
-                    this.showMsg('Возникла ошибка! Сообщение не было отправлено.', true);
+                    this.showTooltip('An error has occurred ! Message was not sent.', true);
                 })
                 .always(function () {
                     $submitBtn.removeAttr('disabled');
@@ -92,49 +122,51 @@
         },
 
         changeBorderRadius: function (event) {
-            this.br = this.$borderRadius.find(':first-child').width() / this.$borderRadius.width() * this.MAX_BORDER_RADIUS ^ 0;
-            this.$resultBtn.css('border-radius', this.br + 'px');
-            this.generateCSS();
+            var that = this;
+
+            that.br = that.$borderRadius.find(':first-child').width() / that.$borderRadius.width() * that.MAX_BORDER_RADIUS ^ 0;
+            that.$resultBtn.css('border-radius', that.br + 'px');
+            that.generateCSS();
         },
 
         changeBorderWidth: function (event) {
-            this.bw = this.$borderWidth.find(':first-child').width() / this.$borderWidth.width() * this.MAX_BORDER_WIDTH ^ 0;
-            this.$resultBtn.css('border-width', this.bw + 'px');
-            this.generateCSS();
+            var that = this;
+
+            that.bw = that.$borderWidth.find(':first-child').width() / that.$borderWidth.width() * that.MAX_BORDER_WIDTH ^ 0;
+            that.$resultBtn.css('border-width', that.bw + 'px');
+            that.generateCSS();
         },
 
         changeButtonText: function (event) {
-            this.$resultBtn.text(this.$btnText.val());
-            this.generateHTML();
+            var that = this;
+
+            that.$resultBtn.text(that.getBtnText());
+            that.generateHTML();
         },
 
-        hideMsg: function () {
-            this.$outputMsg.hide();
+        getBtnText: function () {
+            return this.$btnText.val() || 'Button text';
         },
 
-        showMsg: function (msg, isError) {
-            if (!isError) {
-                this.$outputMsg.removeClass('output__msg--error');
-            } else {
-                this.$outputMsg.addClass('output__msg--error');
-            }
 
-            this.$outputMsg.text(msg).show();
-        },
-
+        /* генерирование HTML, CSS */
         generateHTML: function () {
-            this.$outputHTML.text('<button type="button" class="btn">\r\n\t' + this.$btnText.val() + '\r\n</button>');
+            var that = this;
+
+            that.$outputHTML.text('<button type="button" class="btn">\r\n\t' + that.getBtnText() + '\r\n</button> ');
         },
 
         generateCSS: function () {
-            this.$outputCSS.text(
+            var that = this;
+
+            that.$outputCSS.text(
                 '.btn {' +
                 '\r\n\tcolor: #fff;' +
                 '\r\n\tbackground-color: #37a1e4;' +
                 '\r\n\tbackground-image: -webkit-linear-gradient(top, #77bfed, #4baae6);' +
                 '\r\n\tbackground-image: linear-gradient(to bottom, #77bfed, #4baae6);' +
-                '\r\n\tborder: ' + this.bw + 'px solid #3181b4;' +
-                '\r\n\tborder-radius: ' + this.br + 'px;' +
+                '\r\n\tborder: ' + that.bw + 'px solid #3181b4;' +
+                '\r\n\tborder-radius: ' + that.br + 'px;' +
                 '\r\n\tbox-shadow: 0 1px 2px #999,' +
                 '\r\n\t\tinset 0 1px 2px #fff;' +
                 '\r\n\tdisplay: block;' +
@@ -153,11 +185,33 @@
                 '\r\n\tbackground-image: -webkit-linear-gradient(bottom, #77bfed, #4baae6);' +
                 '\r\n\tbackground-image: linear-gradient(to top, #77bfed, #4baae6);' +
                 '\r\n}');
-        }
+        },
 
+        /* тултип */
+        hideTooltip: function () {
+            this.$outputEmail.tooltip('disable');
+        },
+
+        showTooltip: function (msg, isError) {
+            var that = this,
+                tooltipClass = isError ? 'error' : 'success';
+
+            that.$outputEmail.tooltip('option', 'content', msg);
+            that.$outputEmail.tooltip('option', 'tooltipClass', tooltipClass);
+            that.$outputEmail.tooltip('open');
+        },
+
+        refreshTooltip: function () {
+            var that = this;
+
+            if ($('.ui-tooltip').length) {
+                that.$outputEmail.tooltip('close');
+                that.$outputEmail.tooltip('open');
+            }
+        }
 
     };
 
     app.init();
 
-})(jQuery);
+})(jQuery, window);
